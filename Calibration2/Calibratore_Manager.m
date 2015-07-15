45c8bfe9804a207acabb9b5f6e8719f9b8584296
پدرام داودی <pedram.davoudi@gmail.com>
2015-07-15 05:52:53 +0430

    Revert "Revert "New  Blind Calibrator""
    This reverts commit 7b1249da0372d934fe25da74a0406dcf98d6f8fa.

---
+++ Calibration2/Calibratore_Manager.m
@@ -0,0 +1,41 @@
+clear
+close all
+clc
+%persistent FileName Par_Calib Var_Calib
+
+FileName='example1.mod';
+
+% Minimum % Step %Maximum
+% Par_Calib(1)={'*=1:0.1:6'};
+Par_Calib(1)={'a=-6:0.1:6'};
+%Par_Calib(2)={'c:-6:0.1:6'};
+
+
+%Variance
+Calib.Var=[0,nan;nan,0];
+
+% Variance Weight Matrix
+Weight.Var=[1,1;1,1];
+%Auto Correlatoin of 1 lag 
+Calib.ACorr=[nan,nan;nan,nan];
+Weight.ACorr=[1,1;1,1];
+% Wieght Matrix
+
+% Steady State 
+Calib.SS=[nan,nan];
+Weight.SS=[1,1];
+
+% Wieght Matrix
+
+% Steady State 
+%Calib.Mean=[nan,nan];
+Weight.Mean=[nan,nan];
+% Weight Vector
+
+% Maximum Itration
+MaxIt=1;
+Opt=Calibratore(FileName,Par_Calib,Calib,Weight,MaxIt);
+
+
+% Clear Extra Var
+clearvars -except Opt
